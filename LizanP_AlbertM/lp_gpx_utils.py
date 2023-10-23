08fbe55d4f003dab96cf7670a0306973ec4a7a11
Lizan_Penaranda <pera0044@algonquinlive.com>
2023-10-23 12:54:45 -0400

    gpx exercise completed

--- LizanP_AlbertM/lp_gpx_utils.py
+++ LizanP_AlbertM/lp_gpx_utils.py
@@ -6,3 +6,13 @@ def get_coords_from_gpx(gpx):
         return f'({long_value},{lat_value})'
     else:
         return None, None
+
+def get_coords_from_gpx_first_try(gpx):
+    if "trkpt" in gpx:
+        index_lat = gpx.find("lat=")
+        index_lon = gpx.find("lon=")
+        lat_value = gpx[index_lat+5 : index_lon-2]
+        long_value = gpx[index_lon+5 : len(gpx)-2]
+        return f'({long_value},{lat_value})'
+    else:
+        return None, None
--- LizanP_AlbertM/test_lp_gpx_utils.py
+++ LizanP_AlbertM/test_lp_gpx_utils.py
@@ -11,3 +11,15 @@ def test_get_coords_from_gpx_2():
     expected = (None, None)
     actual = gu.get_coords_from_gpx(gpx)
     assert expected == actual
+
+def test_get_coords_from_gpx_3():
+    gpx = '<trkpt lat="45.38889951234" lon="-75.7472631">'
+    expected = "(-75.7472631,45.38889951234)"
+    actual = gu.get_coords_from_gpx(gpx)
+    assert expected == actual
+
+def test_get_coords_from_gpx_4():
+    gpx = '<trkpt lat="45.3888995" lon="-75.74726311234">'
+    expected = "(-75.74726311234,45.3888995)"
+    actual = gu.get_coords_from_gpx(gpx)
+    assert expected == actual
