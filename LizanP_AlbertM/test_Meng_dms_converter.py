ace9fc0d1a983d1fa4d4cd31e503e079a8ac9545
Lizan_Penaranda <pera0044@algonquinlive.com>
2023-10-26 14:29:12 -0400

    I added additional tests to review results

--- LizanP_AlbertM/test_Meng_dms_converter.py
+++ LizanP_AlbertM/test_Meng_dms_converter.py
@@ -2,25 +2,30 @@ import Meng_dms_converter as dc
 
 def test_converter1():
     expection=None,None
-    actual=dc.dms2dd("91 0 0 N")
+    actual=dc.dms2dd("91 00 00 N")
     assert expection==actual
 
 def test_converter2():
     expection=None,None
-    actual=dc.dms2dd("183 1 2 E 88 22 33 N")
+    actual=dc.dms2dd("183 01 02 E 88 22 33 N")
     assert expection==actual
 
 def test_converter3():
     expection=-100.0,81.0
-    actual=dc.dms2dd("100 0 0 w 81 0 0 N")
+    actual=dc.dms2dd("100 00 00 w 81 00 00 N")
     assert expection==actual
 
 def test_converter4():
     expection=99.8,-23.6
-    actual=dc.dms2dd("99 48 0 E 23 36 0 S")
+    actual=dc.dms2dd("99 48 00 E 23 36 00 S")
     assert expection==actual
 
 def test_converter5():
     expection=-127.78,-86.88
     actual=dc.dms2dd("127 46 48 W 86 52 48 s")
+    assert expection==actual
+
+def test_converter6():
+    expection=-75.75083,45.38472
+    actual=dc.dms2dd("075 45 03 W 45 23 05 N")
     assert expection==actual
--- LizanP_AlbertM/test_Meng_gpx_utils.py
+++ LizanP_AlbertM/test_Meng_gpx_utils.py
@@ -2,11 +2,20 @@ import Meng_gpx_utils as Mgu
 
 def test_get_coords_from_gpx_none():
     expection=None,None
-    actual=Mgu.get_coords_from_gpx('yuuh asc:1111 bnh:9876')
+    actual=Mgu.get_coords_from_gpx('<lat="45.3888995" lon="-75.7472631">')
     assert expection==actual
-
 
 def test_get_coords_from_gpx_coords():
     expection='-75.7472631','45.3888995'
-    actual=Mgu.get_coords_from_gpx('trkpt lat="45.3888995" lon="-75.7472631"')
+    actual=Mgu.get_coords_from_gpx('<trkpt lat="45.3888995" lon="-75.7472631">')
+    assert expection==actual
+
+def test_get_coords_from_gpx_2():
+    expection='-75.7472631','45.38889951234'
+    actual=Mgu.get_coords_from_gpx('<trkpt lat="45.38889951234" lon="-75.7472631">')
+    assert expection==actual
+
+def test_get_coords_from_gpx_3():
+    expection='-75.74726311234','45.3888995'
+    actual=Mgu.get_coords_from_gpx('<trkpt lat="45.3888995" lon="-75.74726311234">')
     assert expection==actual
