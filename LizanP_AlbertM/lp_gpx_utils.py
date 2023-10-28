01e036f3a524553e479f9d8e0a13821163f30ea1
Lizan_Penaranda <pera0044@algonquinlive.com>
2023-10-28 10:24:32 -0400

    code adjusted according to David's comments

--- LizanP_AlbertM/lp_gpx_utils.py
+++ LizanP_AlbertM/lp_gpx_utils.py
@@ -1,9 +1,9 @@
 def get_coords_from_gpx(gpx):
     gpx_list = gpx.split()
     if "trkpt" in gpx_list[0]:
-        lat_value = gpx_list[1].split('"')[1]
-        long_value = gpx_list[2].split('"')[1]
-        return f'({long_value},{lat_value})'
+        lat_value = float(gpx_list[1].split('"')[1])
+        long_value = float(gpx_list[2].split('"')[1])
+        return long_value, lat_value
     else:
         return None, None
 
--- LizanP_AlbertM/test_lp_gpx_utils.py
+++ LizanP_AlbertM/test_lp_gpx_utils.py
@@ -2,24 +2,40 @@ import lp_gpx_utils as gu
 
 def test_get_coords_from_gpx_1():
     gpx = '<trkpt lat="45.3888995" lon="-75.7472631">'
-    expected = "(-75.7472631,45.3888995)"
-    actual = gu.get_coords_from_gpx(gpx)
-    assert expected == actual
+    expected_longitude = -75.7472631
+    expected_latitude = 45.3888995
+    actual_longitude, actual_latitude = gu.get_coords_from_gpx(gpx)
+    assert expected_longitude == actual_longitude
+    assert expected_latitude == actual_latitude
 
 def test_get_coords_from_gpx_2():
     gpx = '<lat="45.3888995" lon="-75.7472631">'
-    expected = (None, None)
-    actual = gu.get_coords_from_gpx(gpx)
-    assert expected == actual
+    expected_longitude = None
+    expected_latitude = None
+    actual_longitude, actual_latitude = gu.get_coords_from_gpx(gpx)
+    assert expected_longitude == actual_longitude
+    assert expected_latitude == actual_latitude
 
 def test_get_coords_from_gpx_3():
-    gpx = '<trkpt lat="45.38889951234" lon="-75.7472631">'
-    expected = "(-75.7472631,45.38889951234)"
-    actual = gu.get_coords_from_gpx(gpx)
-    assert expected == actual
+    gpx = '<trkpt lat="45.3888995" lon="-75.74726311234">'
+    expected_longitude = -75.74726311234
+    expected_latitude = 45.3888995
+    actual_longitude, actual_latitude = gu.get_coords_from_gpx(gpx)
+    assert expected_longitude == actual_longitude
+    assert expected_latitude == actual_latitude
 
 def test_get_coords_from_gpx_4():
-    gpx = '<trkpt lat="45.3888995" lon="-75.74726311234">'
-    expected = "(-75.74726311234,45.3888995)"
-    actual = gu.get_coords_from_gpx(gpx)
-    assert expected == actual
+    gpx = '<trkpt lat="45.38889951234" lon="-75.7472631">'
+    expected_longitude = -75.7472631
+    expected_latitude = 45.38889951234
+    actual_longitude, actual_latitude = gu.get_coords_from_gpx(gpx)
+    assert expected_longitude == actual_longitude
+    assert expected_latitude == actual_latitude
+
+def test_get_coords_from_gpx_5():
+    gpx = '<trkpt lat="45.38889951234" lon="-75.74726311234">'
+    expected_longitude = -75.74726311234
+    expected_latitude = 45.38889951234
+    actual_longitude, actual_latitude = gu.get_coords_from_gpx(gpx)
+    assert expected_longitude == actual_longitude
+    assert expected_latitude == actual_latitude
