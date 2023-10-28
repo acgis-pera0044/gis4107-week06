8742e57695aae38970ea32dc52999360abf3daf1
Lizan_Penaranda <pera0044@algonquinlive.com>
2023-10-28 10:33:40 -0400

    Test 3 and 4 adjusted

--- LizanP_AlbertM/test_lp_gpx_utils.py
+++ LizanP_AlbertM/test_lp_gpx_utils.py
@@ -17,16 +17,16 @@ def test_get_coords_from_gpx_2():
     assert expected_latitude == actual_latitude
 
 def test_get_coords_from_gpx_3():
-    gpx = '<trkpt lat="45.3888995" lon="-75.74726311234">'
+    gpx = '<trkpt lat="-45.3888995" lon="-75.74726311234">'
     expected_longitude = -75.74726311234
-    expected_latitude = 45.3888995
+    expected_latitude = -45.3888995
     actual_longitude, actual_latitude = gu.get_coords_from_gpx(gpx)
     assert expected_longitude == actual_longitude
     assert expected_latitude == actual_latitude
 
 def test_get_coords_from_gpx_4():
-    gpx = '<trkpt lat="45.38889951234" lon="-75.7472631">'
-    expected_longitude = -75.7472631
+    gpx = '<trkpt lat="45.38889951234" lon="75.7472631">'
+    expected_longitude = 75.7472631
     expected_latitude = 45.38889951234
     actual_longitude, actual_latitude = gu.get_coords_from_gpx(gpx)
     assert expected_longitude == actual_longitude
